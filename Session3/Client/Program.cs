4688f866ca61d60414ad0ca749e0748f6cc776d4
Andrei Bostan <abostan.ir@gmail.com>
2020-09-17 11:22:47 +0200

    Made server with multithread

---
+++ Session3/Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Client ....");
+
+            TcpClient client = new TcpClient("127.0.0.1", 8080);
+            NetworkStream stream = client.GetStream();
+
+            string input = "";
+
+            while (input != "exit")
+            {
+                input = Console.ReadLine();
+                byte[] dataToServer = Encoding.ASCII.GetBytes(input);
+                stream.Write(dataToServer, 0, dataToServer.Length);
+
+                byte[] dataFromServer = new byte[1024];
+                int bytesRead = stream.Read(dataFromServer, 0, dataFromServer.Length);
+                string response = Encoding.ASCII.GetString(dataFromServer, 0, bytesRead);
+
+                Console.WriteLine(response);
+            }
+
+            stream.Close();
+            client.Close();
+        }
+    }
+}
---
+++ Session3/Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Server...");
+            
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener listener = new TcpListener(ip, 8080);
+            listener.Start();
+
+            Console.Out.WriteLine("Server started...");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.Out.WriteLine("Client connected");
+                NetworkStream stream = client.GetStream();
+                
+                CommunicationThreadHandler communicationThreadHandler = new CommunicationThreadHandler(stream);
+                
+                //     if (s == "exit")
+                //     {
+                //         break;
+                //     }
+                
+                // close connection
+                // client.Close();
+            }
+        }
+    }
+}
