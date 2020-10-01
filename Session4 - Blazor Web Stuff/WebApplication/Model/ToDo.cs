cc0e16e13f472ee2d10aa60e847a4b374a90cb72
Andrei Bostan <abostan.ir@gmail.com>
2020-10-01 10:11:03 +0200

    Added last session and mock for current one

---
+++ Session4 - Blazor Web Stuff/WebApplication/Model/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Model
+{
+    public class Todo
+    {
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
+        public int UserId { get; set; }
+
+        public int TodoId { get; set; }
+        [Required, MaxLength(128)] public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ Session5/AuthentificationLogic/WebApplication/Data/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public interface IUserService
+    {
+        User ValidateUser(string userName, string Password);
+    }
+}
