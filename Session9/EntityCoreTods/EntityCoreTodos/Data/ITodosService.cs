460d662061e996b5d2c3c879a7453aeea77aa23d
Andrei Bostan <abostan.ir@gmail.com>
2020-11-05 18:35:10 +0100

    refactored the structure

---
+++ Session9/EntityCoreTods/EntityCoreTodos/Data/ITodosService.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdvancedTodoWebAPI.Models;
+
+namespace AdvancedTodoWebAPI.Data {
+public interface ITodosService {
+    Task<IList<Todo>> GetTodosAsync();
+    Task<Todo>   AddTodoAsync(Todo todo);
+    Task   RemoveTodoAsync(int todoId);
+    Task<Todo>   UpdateAsync(Todo todo);
+}
+}
---
+++ Session9/EntityCoreTods/EntityCoreTodos/Persistance/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using AdvancedTodoWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityCoreTodos.Persistance
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = Todos.db");
+        }
+    }
+}
